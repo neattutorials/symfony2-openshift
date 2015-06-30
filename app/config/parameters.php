f5f1074920a842f5b9ca723a02dd2740eae3a94f
aheart <aheart@users.noreply.github.com>
2015-06-30 14:53:47 +0300

    Testing hot deployment

---
+++ app/config/parameters.php
@@ -0,0 +1,10 @@
+<?php
+# app/config/params.php
+if (getenv("OPENSHIFT_MYSQL_DB_HOST")!='') {
+    $container->setParameter('database_host', getenv("OPENSHIFT_MYSQL_DB_HOST"));
+    $container->setParameter('database_port', getenv("OPENSHIFT_MYSQL_DB_PORT"));
+    $container->setParameter('database_name', getenv("OPENSHIFT_APP_NAME"));
+    $container->setParameter('database_user', getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+    $container->setParameter('database_password', getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+}
+?>
